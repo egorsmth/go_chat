38ec617df368242592f3cbfe37740e7417dc8949
Matt182 <mattspring0@gmail.com>
2018-02-07 19:39:12 +0000

    tests'

--- tests/genetator.go
+++ tests/genetator.go
@@ -7,7 +7,15 @@ import (
 func genUser() error {
 	_, err := shared.Db.Exec("insert into auth_user " +
 		"(id, username, password, is_superuser, email, first_name, last_name, is_staff, is_active, date_joined) values " +
-		"(1, 'mattspr', 'password', false, 'matt@spr.com', '', '', false, true, '2018-01-22')")
+		"(1, 'mattspr', 'password', false, 'matt@spr.com', '', '', false, true, '2018-01-22'), " +
+		"(2, 'Zack', 'pswwtf', false, 'zax@mail.com', '', '', false, true, '2018-01-25')")
+	return err
+}
+
+func genPairChatroom() error {
+	_, err := shared.Db.Exec("insert into user_profile_usertopairchatroom " +
+		"(user_id, pair_id, chat_room_id) values " +
+		"(1, 2, 1)")
 	return err
 }
 
@@ -16,7 +24,29 @@ func genChatRooms() error {
 	return err
 }
 
-func clearDb() {
-	_, _ = shared.Db.Exec("DELETE FROM auth_user")
-	_, _ = shared.Db.Exec("DELETE FROM user_profile_chatroom")
+func genMessages() error {
+	_, err := shared.Db.Exec("insert into user_profile_message (id, chat_room_id, message, created_at, user_id, status) values " +
+		"(1, 1, 'first message', '2018-02-07 15:33', 2, 'unread'), (2, 1, 'second message', '2018-02-07 16:11', 1, 'unread')")
+	return err
+}
+
+func clearDb() error {
+	_, err := shared.Db.Exec("DELETE FROM user_profile_message")
+	if err != nil {
+		return err
+	}
+	_, err = shared.Db.Exec("DELETE FROM user_profile_usertopairchatroom")
+	if err != nil {
+		return err
+	}
+	_, err = shared.Db.Exec("DELETE FROM auth_user")
+	if err != nil {
+		return err
+	}
+
+	_, err = shared.Db.Exec("DELETE FROM user_profile_chatroom")
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- tests/models_test.go
+++ tests/models_test.go
@@ -1,16 +1,16 @@
 package tests
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/egorsmth/go_chat/models"
 	"github.com/egorsmth/go_chat/shared"
 )
 
-func setup() {
+func setup() error {
 	err := shared.Init("user=root password=root dbname=test_social_net sslmode=disable")
 	if err != nil {
 		log.Println("Something wrong with db, check django settings", err)
@@ -21,21 +21,38 @@ func setup() {
 		log.Println("gen user", err)
 		return err
 	}
+
 	err = genChatRooms()
 	if err != nil {
 		log.Println("gen chatrooms", err)
 		return err
 	}
+
+	err = genPairChatroom()
+	if err != nil {
+		log.Println("gen user to pair", err)
+		return err
+	}
+
+	err = genMessages()
+	if err != nil {
+		log.Println("gen rmessages", err)
+		return err
+	}
+
 	return nil
 }
 
 func teardown() {
-	clearDb()
+	err := clearDb()
+	if err != nil {
+		log.Println("teardown", err)
+	}
 }
 
 func TestMain(m *testing.M) {
 	err := setup()
-	if err {
+	if err != nil {
 		teardown()
 		log.Fatal("err while set up", err)
 	}
@@ -44,10 +61,66 @@ func TestMain(m *testing.M) {
 	os.Exit(retCode)
 }
 
-func TestChatRoom(*testing.T) {
+var expectedRoom = models.ChatRoom{1, nil, 1, "pair"}
+
+var ID = 1
+var msg = "first message"
+var status = "unread"
+var date = time.Date(2018, 02, 07, 15, 33, 00, 00, time.UTC)
+
+var expectedRoomIds = []int{1}
+
+var expectedUnreaded = 1
+
+func TestChatRoom(t *testing.T) {
 	user := models.User{}
 	id := 1
 	user.ID = &id
-	c, i, err := models.GetChatRooms(&user)
-	fmt.Println(c, i, err)
+	rooms, roomIds, unreaded, err := models.GetChatRooms(&user)
+	if err != nil {
+		t.Error("error", err)
+	}
+
+	if len((*rooms)) != 1 {
+		t.Error("expected", 1, "got", len((*rooms)))
+	}
+
+	if (*rooms)[0].ID != expectedRoom.ID {
+		t.Error("expected", expectedRoom, "got", *rooms)
+	}
+
+	if (*rooms)[0].LastMessageID != expectedRoom.LastMessageID {
+		t.Error("expected", expectedRoom, "got", *rooms)
+	}
+
+	if (*rooms)[0].Status != expectedRoom.Status {
+		t.Error("expected", expectedRoom, "got", *rooms)
+	}
+
+	message := (*rooms)[0].LastMessage
+	if *(*message).ID != ID {
+		t.Error("expected", *(*message).ID, "got", ID)
+	}
+	if *(*message).Message != msg {
+		t.Error("expected", *(*message).Message, "got", msg)
+	}
+	if *(*message).Status != status {
+		t.Error("expected", *(*message).Status, "got", status)
+	}
+
+	if *(*message).Date != date {
+		t.Error("expected", *(*message).Date, "got", date)
+	}
+
+	if len((*roomIds)) != len(expectedRoomIds) {
+		t.Error("expected", roomIds, "got", expectedRoomIds)
+	}
+
+	if (*roomIds)[0] != expectedRoomIds[0] {
+		t.Error("expected", roomIds, "got", expectedRoomIds)
+	}
+
+	if unreaded != expectedUnreaded {
+		t.Error("expected", expectedUnreaded, "got", unreaded)
+	}
 }
